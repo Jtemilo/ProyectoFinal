4d7f7ea718380e7d6abdf34f59c9f6eef4655106
JenifferMorales <jeniffer.morales.201503766@gmail.com>
2022-09-26 19:15:33 -0600

    VistaLogo

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => 'bd_biblioteca_parcial',
+        'database' => '',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
